7ed6d9fd60eb2cf03274c017cbb1c01a4e62ab04
WasaPumpkin <andycarsan10@gmail.com>
2025-07-06 19:18:40 -0500

    message

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello World!</h1>
       <p>This is my Vite + React + TypeScript app</p>
+      <p>This is my Vite + React + TypeScript app</p>
     </div>
   )
 }
