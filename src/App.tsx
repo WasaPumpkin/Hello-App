2aa9e4c1a5d5d0dc6b9de6384f2e7ed884336096
WasaPumpkin <andycarsan10@gmail.com>
2025-07-09 13:26:01 -0500

    Test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hola Mundo!</h1>
+      <h1>Hello from Azure DevOps + S3 🚀</h1>
+      <h1>Hello from Azure DevOps + S3 🚀</h1>
+      <h1>Hello from Azure DevOps + S3 🚀</h1>
       <p>Esta es una aplicacion Vite + React + TypeScript app</p>
       <p>Esta es una aplicacion Vite + React + TypeScript app</p>
       <p>Esta es una aplicacion Vite + React + TypeScript app</p>
- 
     </div>
-  )
+  );
 }
 
 export default App
